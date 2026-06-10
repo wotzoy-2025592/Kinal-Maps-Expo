f3400db27dc33f93700b2b6950fd2ee6d066d54d
jsandoval-2025365 <jsandoval-2025365@kinal.edu.gt>
2026-06-10 15:34:23 -0600

    feat: se ha agregado la parte de las imagenes 360 del recorrido de basicos

---
+++ js/pasillo-basico.js
@@ -0,0 +1,14 @@
+pannellum.viewer('panorama', {
+    type: 'equirectangular', 
+    panorama: 'img/pasillo_basicos.jpg', 
+    autoLoad: true, compass: true, 
+    showZoomCtrl: true,
+    showFullscreenCtrl: true, 
+    hfov: 75, 
+    minHfov: 40, 
+    maxHfov: 110, 
+    pitch: 0, 
+    yaw: 0, 
+    utoRotate: 0, 
+    mouseZoom: true
+});
